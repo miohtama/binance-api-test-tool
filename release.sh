8cbfa18c792800f3b06ffc0978fa9d2bb81ffdf3
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-04-23 00:14:51 +0200

    Set up DockerHub

--- release.sh
+++ release.sh
@@ -3,17 +3,23 @@
 # Create a Docker Hub release
 #
 
+set -e
+set -u
+
+export BINANCE_NETWORK=spot-testnet
+
 IMAGE="miohtama/binance-api-test-tool"
 
 VERSION=`python -c "import binance_testnet_tool ; print(binance_testnet_tool.__version__)"`
 
+echo "Preparing a release for version $VERSION"
 docker build -t miohtama/binance-api-test-tool:latest .
 
 # Test run - smoke test will probably exit non-zero if Python dependencies failed
-echo "Docker local version is now " && docker run $IMAGE:latest version
+echo "Docker local version is now " && docker run -e BINANCE_NETWORK $IMAGE:latest version
 # Push the release to hub
-docker tag $IMAGE:latest $IMAGE:$(VERSION)
+docker tag $IMAGE:latest $IMAGE:$VERSION
 
 # Update the latest and the tag
-docker push $IMAGE:$(VERSION)
+docker push $IMAGE:$VERSION
 docker push $IMAGE:latest
