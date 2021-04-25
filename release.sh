2621d75e4d27acc565352bec201a26aba36c28fb
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-04-25 18:43:29 +0200

    Cleanup versioned .pyc. Updated the release script.

--- release.sh
+++ release.sh
@@ -17,16 +17,20 @@ echo "Preparing a release for version $VERSION"
 # https://stackoverflow.com/a/66420668/315168
 read -p "Continue? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-git tag -a v$VERSION -m "Releasing new version with release.sh - $VERSION"
-git push master && git push master --tags
-
+# Build
 docker build -t miohtama/binance-api-test-tool:latest .
 
-# Test run - smoke test will probably exit non-zero if Python dependencies failed
+# Smoke test
 echo "Docker local version is now " && docker run -e BINANCE_NETWORK $IMAGE:latest version
+
+git tag -a v$VERSION -m "Releasing new version with release.sh - $VERSION"
+git push origin master && git push origin master --tags
+
 # Push the release to hub
 docker tag $IMAGE:latest $IMAGE:$VERSION
 
 # Update the latest and the tag
 docker push $IMAGE:$VERSION
 docker push $IMAGE:latest
+
+echo "Release $VERSION done"
