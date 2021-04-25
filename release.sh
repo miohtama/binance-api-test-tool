73da42eb5577c493930e1e2e80a44c0d22f07ff2
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-04-25 18:37:17 +0200

    Updated the release script.

--- release.sh
+++ release.sh
@@ -6,13 +6,20 @@
 set -e
 set -u
 
+# Needed for the smoke test of Docker image
 export BINANCE_NETWORK=spot-testnet
 
 IMAGE="miohtama/binance-api-test-tool"
 
 VERSION=`python -c "import binance_testnet_tool ; print(binance_testnet_tool.__version__)"`
-
 echo "Preparing a release for version $VERSION"
+
+# https://stackoverflow.com/a/66420668/315168
+read -p "Continue? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+git tag -a v$VERSION -m "Releasing new version with release.sh - $VERSION"
+git push master && git push master --tags
+
 docker build -t miohtama/binance-api-test-tool:latest .
 
 # Test run - smoke test will probably exit non-zero if Python dependencies failed
